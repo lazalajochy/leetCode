11c68101c61a957e1597b1255df6a451252b209d
lazalajochy <lazala1401jochy@outlook.com>
2023-07-02 14:53:05 -0400

    array prototype

---
+++ arrayPrototype/arrayPrototype.js
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/array-prototype-last/description/
+
+
+Array.prototype.last = function() {
+    return (this.length > 0) ? this[this.length -1] : -1
+    
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
